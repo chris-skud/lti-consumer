f65f40e7c7f7c5e1e07288bf27c87ed221c6576d
chris-skud <chris.skud@gmail.com>
2016-06-03 11:20:28 -0600

    Enable client to pass express app.

--- examples/index.js
+++ examples/index.js
@@ -1,2 +1,20 @@
+var express = require('express')
+var app = express()
+
+var port = process.env.PORT || 8090
 var ltiConsumer = require('../lib/lti.js')
-ltiConsumer.init(null, {consumer: true})
+var storage = require('../lib/storage/jfs.js')(null)
+var options = {port: port, storage: storage}
+
+ltiConsumer.init(app, options)
+
+app.listen(port, function (err) {
+    if (err) {
+      return console.error('Error starting server: ', err)
+    }
+
+    console.log('Server successfully started on port %s', port)
+  })
+
+// you can also delegate creation of express web server to lti-consumer module
+// ltiConsumer.init(null, {})
