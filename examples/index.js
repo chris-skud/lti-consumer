d05ecc30ac8a2dcb92d2b2c5b1513924ccaa7b24
chris-skud <chris.skud@gmail.com>
2016-06-03 16:53:45 -0600

    Bunch o formatting.

--- examples/index.js
+++ examples/index.js
@@ -9,12 +9,12 @@ var options = {port: port, storage: storage}
 ltiConsumer.init(app, options)
 
 app.listen(port, function (err) {
-    if (err) {
-      return console.error('Error starting server: ', err)
-    }
+  if (err) {
+    return console.error('Error starting server: ', err)
+  }
 
-    console.log('Server successfully started on port %s', port)
-  })
+  console.log('Server successfully started on port %s', port)
+})
 
 // you can also delegate creation of express web server to lti-consumer module
 // ltiConsumer.init(null, {})
