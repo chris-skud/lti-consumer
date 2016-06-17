f8ec4b830ae7a801f5060ab9498aaf0e20790212
chris-skud <chris.skud@gmail.com>
2016-06-16 23:39:54 -0600

    Modify how lit is wired in.

--- examples/simple/index.js
+++ examples/simple/index.js
@@ -1,16 +1,26 @@
 var express = require('express')
 var app = express()
 
+var lti = require('../../app.js')
+app.use(lti)
+
+app.engine('.html', require('ejs').__express)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'html')
+
 var port = process.env.PORT || 8090
 var storage = require('../../lib/storage/jfs.js')(null)
-var LTIConsumer = require('../../lib/lti_consumer.js')
 
-var ltiConsumer = new LTIConsumer(app, storage)
+// perhaps something like this
+// ltiConsumer.on('grade', function () {
+//   console.log('connected to resource server')
+// })
 
 // imagine this is the handler for a link presented in the LMS
-app.route('/tool/:id')
-  .post(function (req, res) {
-    var ldata = ltiConsumer.launchData(require('../launch_params.js'))
+app.route('/lms')
+  .get(function (req, res) {
+    var launchParamDefaults = require('../launch_params.js')
+    res.render('index', { launchParams: launchParamDefaults })
   })
 
 app.listen(port, function (err) {
